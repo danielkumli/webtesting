de33beb629efb85dcbb2f879cb0f33b246f807a7
Daniel Kumli <116157161+danikumli@users.noreply.github.com>
2026-06-15 12:13:45 +0200

    go to shopping cart test (#1)

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.goto('https://react-shopping-cart-67954.firebaseapp.com/')
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Go to shopping cart', async ({ page }) => {
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.goto('https://react-shopping-cart-67954.firebaseapp.com/');
+  await page.getByTitle('Products in cart quantity').click();
+  await expect (page.getByRole('button', {name :'CHECKOUT'})).toBeVisible();
+  });
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  test('Verify Sizes', async ({page}) => {
+
+
+  });
